a6b2a2481b6bbdda4d778acb96bd0934b6873ebf
Clenill <jokerforpeace@gmail.com>
2026-05-10 11:43:22 +0200

    Modificato il metodo main

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from contextlib import asynccontextmanager
 
 from app.db.database import engine
 from app.db.models import Base
+from app.api.routes import router
 
-
-app = FastAPI()
-
-app.include_router(router, prefix="/api")
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    print("\n===== SERVER AVVIATO =====")
+    print("Home: http://127.0.0.1:8000/api/")
+    print("Swagger Docs: http://127.0.0.1:8000/docs")
+    print("ReDoc: http://127.0.0.1:8000/redoc")
+    print("==========================\n")
+
+    yield
+
+    # Shutdon
+    print("Server spento")
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router, prefix="/api")
