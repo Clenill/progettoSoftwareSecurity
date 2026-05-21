14e599564f801a84fb73bcf345e731a8fe0579c7
Clenill <jokerforpeace@gmail.com>
2026-05-21 09:55:20 +0200

    Rimosso codice e spostato nel req.txt

--- app/core/security.py
+++ app/core/security.py
@@ -18,11 +18,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Dove FastAPI deve cercare il token
 oauth2_scheme = APIKeyHeader(name="Authorization", auto_error=False)
 
-# Configurazione DA AGGIUNGERE AL FILE .env
-# SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
